9df956829897a30fef14b8dfe752b88385e6f999
morris <akunnemorris@gmail.com>
2024-07-09 22:33:47 +0100

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyparser = require ('body-parser');
 const pg = require("pg");
 //const consolidate = require("consolidate");
-const { Sequelize } = require('sequelize');
+const { Sequelize, UnknownConstraintError, where } = require('sequelize');
 const {ulid} = require("ulid");
 const jwt = require("jsonwebtoken");
 const cookieparser = require("cookie-parser");
@@ -22,7 +22,7 @@ app.use(cookieparser());
 
 
 // Option 3: Passing parameters separately (other dialects)
-const DB = new Sequelize('hng-stage2', 'morris', '*****', {
+const DB = new Sequelize('hng-stage2', 'morris', process.env.DB_PASSKEY, {
   host: 'localhost',
   dialect:  'postgres',
   
@@ -71,7 +71,7 @@ const User = DB.define("User", {
 
 });
 
-const Organization = DB.define("organization", {
+const Organization = DB.define("Organization", {
 orgId:{
     type: Sequelize.STRING, 
     required: true,
@@ -95,53 +95,104 @@ app.get("/login", (req,res) => {
 res.sendFile(__dirname + "/login.html")
 
 })
-app.get("/api/users/:id", (req, res)=> {
+app.get("/api/users/:id",async (req, res)=> {
+    try {
+    const id = req.params.id;
+   // const user_id = req.user.userId;
+    console.log(id);
+    //console.log(user_id);
+    const user = await User.findOne({
+        where: {userId: id}
+    })
+    console.log(user);
+    const organisation = await Organization.findOne({
+        where: {userId: id}
+    })
+    if (!user) {
+     res.status(404).json({
+        status: "error",
+        message: " no user found",
+        statusCode:"404"
+    });
+
+    } else {
+        res.send({
+            status: "success",
+            message: "user found",
+            data:{
+                userId: id,
+                firstName: user.firstname,
+                lastName: user.lastname,
+                email: user.email,
+                phone: user.phone,
+                organisation
+            }
+        })
     
+    }
+} catch (error) {
+    res.json({
+        status: error,
+        message:"Server error",
+        statusCode: res.statusCode
+    })  
+}
+
+
 });
-app.post("/auth/register", (req, res) => {
+
+app.post("/auth/register", async (req, res) => {
 const firstname = req.body.firstname;
 const lastname = req.body.lastname;
 const email = req.body.email;
 const phone = req.body.phone;
- 
-// hashing password field
-// bcrypt.hash(req.body.password, saltRound).then((hash)=> {
-// User.create({
-//     firstname,
-//     lastname,
-//     email,
-//     password: hash,
-//     phone
-// }).then((data)=>{
-//     const token = jwt.sign(data.userId , "secret",{
+const foundUser = await User.findAll({where: {email: email}})
+
+console.log(req.user);
+ if (foundUser.length > 0) {
+    res.send("email already exits try again with a different email")
+ } else {
+   //hashing password field
+   
+bcrypt.hash(req.body.password, saltRound).then((hash)=> {
+User.create({
+    firstname,
+    lastname,
+    email,
+    password: hash,
+    phone
+}).then((data)=>{
+    const token = jwt.sign(data.userId , "secret",{
         
-//      });
-//      res.cookie("jwt", token, {httpOnly: true})
-// res.json({
-//     status: "success",
-//     message:"Registrstion successful",
-//     data:{accessToken: token,
-//         user:{
-//             userid: data.userId,
-//             firstName: firstname,
-//             lastName: lastname,
-//             email: email,
-//             phone: phone
+     });
+     res.cookie("jwt", token, {httpOnly: true})
+res.json({
+    status: "success",
+    message:"Registrstion successful",
+    data:{accessToken: token,
+        user:{
+            userId: data.userId,
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            phone: phone
 
-//         }
-//     }
+        }
+    }
 
-// })
-// .catch((err)=>{ console.log(err)
-//     res.json({
-//         status: "Bad request",
-//         message:"Registration unsuccessful",
-//         statusCode: res.statusCode
-//     })
-// });
+})
+.catch((err)=>{ console.log(err)
+    res.json({
+        status: "Bad request",
+        message:"Registration unsuccessful",
+        statusCode: res.statusCode
+    })
+});
 
-// })
-// })
+})
+})
+ }
+
 });
 app.post("/auth/login", async (req, res) => {
 try {
@@ -150,12 +201,36 @@ try {
        const password = await bcrypt.compare(req.body.password,element.password)
        if (element.email==req.body.email && password == true ) {
         console.log("good")
-        return user;
+        //return user;
+         token = jwt.sign(element.userId , "secret",{
+        
+                  });
+                 res.cookie("jwt", token, {httpOnly: true})
+        res.json({
+                status: "success",
+                message:"Login successful",
+                data:{accessToken: token,
+                    user:{
+                        userId: element.userId,
+                        firstName: element.firstname,
+                        lastName: element.lastname,
+                        email: element.email,
+                        phone: element.phone,
+                       statusCode: res.statusCode
+            
+                    }
+                }
+            
+             })
        }
     })   
     
 } catch (error) {
-    
+    res.json({
+                status: "Bad request",
+                message:"Authentication failed",
+                statusCode: res.statusCode
+            })
 }
 
 //User.findAll().then((data)=> console.log(data))
